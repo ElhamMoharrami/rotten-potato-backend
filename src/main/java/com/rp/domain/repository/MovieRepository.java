b3af373bfff9e99f6e1c7ed9ab2b78c4e1dbbe74
Taher Khorshidi <taher.khorshidi@ing.com>
2022-07-26 14:04:46 +0200

    Fix CORS

--- src/main/java/com/rp/domain/repository/MovieRepository.java
+++ src/main/java/com/rp/domain/repository/MovieRepository.java
@@ -3,7 +3,9 @@ package com.rp.domain.repository;
 import com.rp.domain.model.Movie;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "*")
 public interface MovieRepository extends PagingAndSortingRepository<Movie, String> {
 }
