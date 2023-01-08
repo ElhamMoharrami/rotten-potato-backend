ae099bb13afb25330196f1d951c94cfdf7e54aff
ghamilouie <30733190+ghamilouie@users.noreply.github.com>
2023-01-08 17:24:16 +0100

    Update UserRepository.java

--- src/main/java/com/rp/domain/repository/UserRepository.java
+++ src/main/java/com/rp/domain/repository/UserRepository.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "*")
 public interface UserRepository extends PagingAndSortingRepository<User, String> {
-
+    @RestResource(path = "byUserName")
     User findByUsername(String username);
 }
